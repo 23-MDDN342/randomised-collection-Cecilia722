0e987d8d438e79dc7f5d389a3c52234e6017ffc8
Cecilia722 <114236944+Cecilia722@users.noreply.github.com>
2024-05-02 14:47:57 +1200

    arrangement

--- arrangement.js
+++ arrangement.js
@@ -80,7 +80,7 @@ function draw() {
   let petalWide_value = random(0, 100);
   let petalNumber_value = random(0, 100);
 
-  let random_daisyFace_size_Fac = map(petalNumber_value, 0, 100, 30, 24)
+  let random_daisyFace_size_Fac = random(10,24)
   let random_DaisyX = random(550, 750)
   let random_DaisyY = random(230, 290)
 
@@ -92,12 +92,11 @@ function draw() {
   let mouth_switch_value = int(random(1, 3))
 
 
-  petalHight_value = random(0, 100);
-  petalWide_value = random(0, 100);
 
-  random_BerryX = random(200, 380)
-  random_BerryY = random(180, 240)
-  random_Berry_size_Fac = random(30, 20)
+
+  let random_BerryX = random(200, 380)
+ let  random_BerryY = random(180, 240)
+ let  random_Berry_size_Fac = random(17,10)
 
 
   let bigLeaf_H_value = random(0, 100);
@@ -108,110 +107,67 @@ function draw() {
 
   let random_LeafX = random(450, 490)
   let random_LeafY = random(120, 280)
-  let random_Leaf_size_Fac = random(20, 15)
+  let random_Leaf_size_Fac = random(10,8)
 
-  
+
+  let branch_Stroke = 1
+
+  let daisyBranch_Offest = random(200,150)
+  let daisyBranchX = random_DaisyX + daisyBranch_Offest
+  let daisyBranchY = random_DaisyY - daisyBranch_Offest
+
+
+  let StrawBerry_Offest = random(-150,-100)
+  let StrawBerryBranchX = random_BerryX + StrawBerry_Offest
+  let StrawBerryBranchY = random_BerryY - StrawBerry_Offest
+
+  let Leaf_Offest = random(100, 150)
+  let LeafBranchX = random_LeafX - Leaf_Offest
+  let LeafBranchY = random_LeafY - Leaf_Offest
+
+
+
 
   noFill()
   stroke(color_DarkGreen)
-  strokeWeight(2)
+  strokeWeight(19)
   push()
 
- rect(random_BerryX, random_BerryY,100)
- rect( random_LeafX, random_LeafY,100)
- rect( random_DaisyX, random_DaisyY,100)
- 
+
+  //the long vine
   beginShape();
 
-  // Add the first control point and draw a segment to it.
-  curveVertex(0,500);
+
+  curveVertex(0, 500);
 
 
-  // Add the anchor points.
-  curveVertex(random_BerryX*0, random_BerryY*1.1);
-  curveVertex(random_BerryX*0.4, random_BerryY*0.7);
-  curveVertex(random_BerryX*0.6, random_BerryY*1.1);
+  curveVertex(random_BerryX * 0, random_BerryY * 1.1);
+  curveVertex(random_BerryX * 0.4, random_BerryY * 0.7);
+  curveVertex(random_BerryX * 0.6, random_BerryY * 1.1);
 
   curveVertex(random_BerryX, random_BerryY);//berry Position
 
-  curveVertex(random_BerryX+random_LeafX/10, random_BerryY+random_LeafY/random(8,10));
+  curveVertex(random_BerryX + random_LeafX / 10, random_BerryY + random_LeafY / random(8, 10));
 
   curveVertex(random_LeafX, random_LeafY,);//leaf position
 
-  curveVertex(random_LeafX+random_DaisyX/7, random_LeafY-random_DaisyY/random(9,20));
+  curveVertex(random_LeafX + random_DaisyX / 7, random_LeafY - random_DaisyY / random(9, 20));
 
   curveVertex(random_DaisyX, random_DaisyY);
 
-  curveVertex(random_DaisyX*1.2, random_DaisyY*1.1);
-  curveVertex(random_DaisyX*1.3, random_DaisyY*0.5);
-  curveVertex(random_DaisyX*1.6, random_DaisyY*0);
+  curveVertex(random_DaisyX * 1.2, random_DaisyY * 1.1);
+  curveVertex(random_DaisyX * 1.3, random_DaisyY * 0.5);
+  curveVertex(random_DaisyX * 1.6, random_DaisyY * 0);
 
 
 
-  curveVertex(960,0);
+  curveVertex(960, 0);
 
 
   endShape();
 
 
-  // beginShape();
-  // vertex(0, 0)
-  // bezierVertex(50, -0, 50, 25, -random_BerryX / 6, random_BerryY);
 
-
-  // endShape();
-  // beginShape();
-  // vertex(0, 0)
-  // bezierVertex(-30, 10, -20, 25, -random_DaisyX / 6, random_DaisyY);
-  // endShape();
-
-
-  // beginShape();
-  // vertex(0, 0)
-  // bezierVertex(10, -0, 40, 25, -random_LeafX / 6, random_LeafY,);
-
-
-  // endShape();
-
-
-  
- pop()
-
-
-
-  stroke(color_DarkGreen)
-  strokeWeight(2)
-  ellipse(0, 0, 100, 100)
-
-
-
-  strokeWeight(1.5)
-
-
-
-  push()
-
-  translate(random_DaisyX, random_DaisyY);
-  scale(w / random_daisyFace_size_Fac, h / random_daisyFace_size_Fac);
-
-
-  rotate(random(-20,10))
-  daisyFace(tilt_value, eye_value, mouth_value, petalHight_value, petalWide_value, petalNumber_value)
-  pop();
-
-
-
-  push()
-
-  translate(random_BerryX, random_BerryY)
-  scale(w / random_Berry_size_Fac, h / random_Berry_size_Fac)
-
-
-  rotate(random(100, 170))
-
-  strawBerry(thinness_value, leafHight_value, leafAngle_value, eye_switch_value, mouth_switch_value, petalHight_value, petalWide_value);
-  rect(random_BerryX, random_BerryY,100)
-  
   pop()
 
 
@@ -220,21 +176,104 @@ function draw() {
 
 
 
+  
+
+
+  //The Daisy
+  push()
+
+  translate(random_DaisyX, random_DaisyY);
+  scale(w / random_daisyFace_size_Fac, h / random_daisyFace_size_Fac);
 
 
 
+  strokeWeight(branch_Stroke*1.7)//branch stroke
+  //Daisy Branch
+  beginShape();
+  curveVertex(0, 0);
+  curveVertex(daisyBranch_Offest * random_daisyFace_size_Fac / w, -daisyBranch_Offest * random_daisyFace_size_Fac / h,);
+
+  curveVertex(5, random(-10,-20));
+  curveVertex(0, 0);
+  curveVertex(0, 0);
+  endShape();
+  pop()
+
+
+
+  //Daisy Face
+  push()
+  translate(daisyBranchX, daisyBranchY);
+  scale(w / random_daisyFace_size_Fac, h / random_daisyFace_size_Fac);
+  rotate(random(-20, 10))
+  daisyFace(tilt_value, eye_value, mouth_value, petalHight_value, petalWide_value, petalNumber_value)
+  pop();
+
+
+
+  //The StrawBerry
+  push()
+  translate(random_BerryX, random_BerryY)
+  scale(w / random_Berry_size_Fac, h / random_Berry_size_Fac)
+
+
+ 
+  strokeWeight(branch_Stroke)//branch stroke
+
+  //StrawBerry Branch
+  beginShape();
+  curveVertex(0, 0);
+  curveVertex(StrawBerry_Offest * random_Berry_size_Fac / w, -StrawBerry_Offest * random_Berry_size_Fac / h,);
+
+  curveVertex(random(0,2), random(6,10));
+  curveVertex(0, 0);
+  curveVertex(0, 0);
+  endShape();
+  pop()
+
+  //StrawBerry Face
+  push()
+  translate(StrawBerryBranchX, StrawBerryBranchY);
+  scale(w / random_Berry_size_Fac, h / random_Berry_size_Fac)
+  rotate(random(100, 170))
+  strawBerry(thinness_value, leafHight_value, leafAngle_value, eye_switch_value, mouth_switch_value, petalHight_value, petalWide_value);
+  pop()
+
+
+
+
+
+
+  //The Leaf
+  push()
 
 
   translate(random_LeafX, random_LeafY)
   scale(w / random_Leaf_size_Fac, h / random_Leaf_size_Fac)
-  strokeWeight(1.5)
 
 
+  strokeWeight(branch_Stroke)//branch stroke
 
 
-  rotate(random(0, 360))
+  //Leaf Branch
+  beginShape();
+  curveVertex(0, 0);
+  curveVertex(-Leaf_Offest * random_Leaf_size_Fac / w, -Leaf_Offest * random_Leaf_size_Fac / h,);
+  
+  curveVertex(random(-2, 1), random(-9, -10));
+  curveVertex(0, 0);
+  curveVertex(0, 0);
+  endShape();
+  pop()
+
+  //Leaf Random
+  push()
+  translate(LeafBranchX, LeafBranchY)
+  scale(w / random_Leaf_size_Fac, h / random_Leaf_size_Fac)
+  rotate(random(-170, -110))
   bigLeaf(bigLeaf_H_value, bigLeaf_W_value, heart_W_value, heart_H_value, bigLeaf_Angle_value)
-   pop()
+
+  pop()
 
 
 
--- face_code.js
+++ face_code.js
@@ -30,41 +30,41 @@ let color_PeachYellow = color("#edd596")
 
 
 
- function heartshape(heart_W_value,heart_H_value){
+function heartshape(heart_W_value, heart_H_value) {
 
-  let heart_W=map(heart_W_value,0,100,2,8)
-  let heart_H=map(heart_H_value,0,100,2,5)
+  let heart_W = map(heart_W_value, 0, 100, 2, 8)
+  let heart_H = map(heart_H_value, 0, 100, 2, 5)
 
   beginShape();
-  vertex(0, -heart_H/2);
-  bezierVertex(heart_W/20, -heart_H*2, heart_W*1.2, -heart_H*2, 0, 0);
-  vertex(0, -heart_H/2);
-  bezierVertex(-heart_W/20, -heart_H*2, -heart_W*1.2, -heart_H*2, 0, 0);
+  vertex(0, -heart_H / 2);
+  bezierVertex(heart_W / 20, -heart_H * 2, heart_W * 1.2, -heart_H * 2, 0, 0);
+  vertex(0, -heart_H / 2);
+  bezierVertex(-heart_W / 20, -heart_H * 2, -heart_W * 1.2, -heart_H * 2, 0, 0);
   vertex(0, -3);
   endShape();
 
-  bezier(0,0, -heart_W/2, -heart_H*3, heart_W/2, -heart_H*3, 0, 0);
+  bezier(0, 0, -heart_W / 2, -heart_H * 3, heart_W / 2, -heart_H * 3, 0, 0);
 
 
- }
+}
 
-function daisyFace(tilt_value, eye_value, mouth_value,petalHight_value,petalWide_value,petalNumber_value) {
+function daisyFace(tilt_value, eye_value, mouth_value, petalHight_value, petalWide_value, petalNumber_value) {
 
-  scale(1,1);
+  scale(1, 1);
   // const bg_color3 = [71, 222, 100];
   // const fg_color3 = [255, 93, 35];
 
   let headSize = 9
   let eyeSize = 1;
   let centerX = 0;
-  let Iy =0
+  let Iy = 0
   let distactBetweenEyes = 2
-  let MouthDrop = map(mouth_value,0,100,2,10)
-  let Mouth_W=random(0.7,2)
+  let MouthDrop = map(mouth_value, 0, 100, 2, 10)
+  let Mouth_W = random(0.7, 2)
 
-  let pistilSize=2
-  let pistilNumber=9
-  let petalNumber=int(map(petalNumber_value,0,100,3,6))
+  let pistilSize = 2
+  let pistilNumber = 9
+  let petalNumber = int(map(petalNumber_value, 0, 100, 3, 6))
 
   let color_DarkGreen = color("#42421c")
   let color_ResedaGreen = color("#84805a")
@@ -86,14 +86,14 @@ function daisyFace(tilt_value, eye_value, mouth_value,petalHight_value,petalWide
   angleMode(DEGREES);
   rotate(tilt_value);
 
-//petals
-  for(let i = 0;i<petalNumber;i++){
+  //petals
+  for (let i = 0; i < petalNumber; i++) {
 
-  
+
     fill(color_White)
-    petal(petalHight_value,petalWide_value)
-  
-    rotate(360/petalNumber)
+    petal(petalHight_value, petalWide_value)
+
+    rotate(360 / petalNumber)
   }
 
   // face & pistil
@@ -101,18 +101,18 @@ function daisyFace(tilt_value, eye_value, mouth_value,petalHight_value,petalWide
   fill(color_Yellow);
   ellipse(centerX, 0, headSize, headSize);
 
-  for(let i = 0;i<pistilNumber;i++){
+  for (let i = 0; i < pistilNumber; i++) {
 
 
 
-  
+
     fill(color_PeachYellow)
 
-    
-    ellipse(0, headSize/2.3, pistilSize, pistilSize);
-  
-    rotate(360/pistilNumber)
- 
+
+    ellipse(0, headSize / 2.3, pistilSize, pistilSize);
+
+    rotate(360 / pistilNumber)
+
   }
 
 
@@ -123,20 +123,20 @@ function daisyFace(tilt_value, eye_value, mouth_value,petalHight_value,petalWide
   // 2 sharp eyes with nose
   if (eye_value === 1 || eye_value == 3) {
     fill(color_IndianRed)
-    ellipse(centerX, Iy, eyeSize+1,eyeSize);
+    ellipse(centerX, Iy, eyeSize + 1, eyeSize);
 
     fill(color_Brown);
 
-  
 
-    arc(centerX - distactBetweenEyes, Iy-0.5, eyeSize*1.5,eyeSize*1.5, 0, 180);
-    arc(centerX + distactBetweenEyes, Iy-0.5, eyeSize*1.5,eyeSize*1.5, 0, 180);
+
+    arc(centerX - distactBetweenEyes, Iy - 0.5, eyeSize * 1.5, eyeSize * 1.5, 0, 180);
+    arc(centerX + distactBetweenEyes, Iy - 0.5, eyeSize * 1.5, eyeSize * 1.5, 0, 180);
 
   }
   // tow round eyes
   if (eye_value >= 2) {
     fill(color_Brown);
-    
+
     ellipse(centerX - distactBetweenEyes, Iy, eyeSize);
     ellipse(centerX + distactBetweenEyes, Iy, eyeSize);
   }
@@ -151,7 +151,7 @@ function daisyFace(tilt_value, eye_value, mouth_value,petalHight_value,petalWide
     noFill()
     beginShape();
     vertex(-Mouth_W, 2);
-    vertex(centerX, Iy + 0.5*MouthDrop);
+    vertex(centerX, Iy + 0.5 * MouthDrop);
     vertex(Mouth_W, 2);
     endShape();
 
@@ -160,20 +160,20 @@ function daisyFace(tilt_value, eye_value, mouth_value,petalHight_value,petalWide
   if (mouth_value >= 2) {
 
 
-  strokeWeight(0.5)
-  strokeJoin(ROUND)
-  stroke(color_IndianRed);
-  fill(color_IndianRed);
-  beginShape();
+    strokeWeight(0.5)
+    strokeJoin(ROUND)
+    stroke(color_IndianRed);
+    fill(color_IndianRed);
+    beginShape();
 
 
-  arc(centerX, Iy+1, Mouth_W*2, Iy + MouthDrop, 0, 180);
-  line(-Mouth_W,Iy+1,Mouth_W,Iy+1)
-  endShape();
+    arc(centerX, Iy + 1, Mouth_W * 2, Iy + MouthDrop, 0, 180);
+    line(-Mouth_W, Iy + 1, Mouth_W, Iy + 1)
+    endShape();
 
-  noStroke()
-  fill(color_White)
-  rect(-Mouth_W/20, Iy+1, Mouth_W, Iy + MouthDrop/5)
+    noStroke()
+    fill(color_White)
+    rect(-Mouth_W / 20, Iy + 1, Mouth_W, Iy + MouthDrop / 5)
   }
 
 
@@ -181,16 +181,16 @@ function daisyFace(tilt_value, eye_value, mouth_value,petalHight_value,petalWide
 
 
 
-function petal (petalHight_value,petalWide_value){
+function petal(petalHight_value, petalWide_value) {
   noStroke()
-  let petalHight=map(petalHight_value,0,100,9,13)
-  let petalWide=map(petalWide_value,0,100,10,12)
-  bezier(0, 0,  -petalWide, -petalHight,  petalWide, -petalHight, 0, 0);
+  let petalHight = map(petalHight_value, 0, 100, 9, 13)
+  let petalWide = map(petalWide_value, 0, 100, 10, 12)
+  bezier(0, 0, -petalWide, -petalHight, petalWide, -petalHight, 0, 0);
 
- }
+}
 
 
-function  bigLeaf(bigLeaf_H_value,bigLeaf_W_value,heart_W_value,heart_H_value,bigLeaf_Angle_value){
+function bigLeaf(bigLeaf_H_value, bigLeaf_W_value, heart_W_value, heart_H_value, bigLeaf_Angle_value) {
 
   let color_DarkGreen = color("#42421c")
   let color_ResedaGreen = color("#84805a")
@@ -205,11 +205,11 @@ function  bigLeaf(bigLeaf_H_value,bigLeaf_W_value,heart_W_value,heart_H_value,bi
 
 
 
-  let heartLeaf_pointY=0
-  let bigLeaf_Angle=map(bigLeaf_Angle_value,0,100,90,140)
+  let heartLeaf_pointY = 0
+  let bigLeaf_Angle = map(bigLeaf_Angle_value, 0, 100, 90, 140)
 
-  let leaf_size_2=map(bigLeaf_H_value,0,100,0.4,0.8)
-  let leaf_size_3=map(bigLeaf_H_value,0,100,0.1,0.39)
+  let leaf_size_2 = map(bigLeaf_H_value, 0, 100, 0.4, 0.8)
+  let leaf_size_3 = map(bigLeaf_H_value, 0, 100, 0.1, 0.39)
 
   // let leaf_size_2=random(0.4,0.8)
   // let leaf_size_3=random(0.1,0.39)
@@ -219,86 +219,86 @@ function  bigLeaf(bigLeaf_H_value,bigLeaf_W_value,heart_W_value,heart_H_value,bi
   strokeJoin(ROUND);
   strokeWeight(1)
   stroke(color_ResedaGreen)
-  fill(color_ResedaGreen )
-
-  
-
-  
-push()
-translate(0,heartLeaf_pointY)
-scale(1,1)
-heartshape(heart_W_value,heart_H_value)
-pop()
-push()
-rotate(bigLeaf_Angle)
-heartshape(heart_W_value,heart_H_value)
-pop()
-push()
-rotate(-bigLeaf_Angle)
-heartshape(heart_W_value,heart_H_value)
-pop()
+  fill(color_ResedaGreen)
 
 
 
 
-
-stroke(color_DarkGreen)
-fill(color_DarkGreen )
-
-push()
-translate(0,heartLeaf_pointY)
-scale(leaf_size_2,leaf_size_2)
-heartshape(heart_W_value,heart_H_value)
-pop()
-
-push()
-
-rotate(bigLeaf_Angle)
-scale(leaf_size_2,leaf_size_2)
-heartshape(heart_W_value,heart_H_value)
-pop()
-
-push()
-rotate(-bigLeaf_Angle)
-scale(leaf_size_2,leaf_size_2)
-heartshape(heart_W_value,heart_H_value)
-pop()
+  push()
+  translate(0, heartLeaf_pointY)
+  scale(1, 1)
+  heartshape(heart_W_value, heart_H_value)
+  pop()
+  push()
+  rotate(bigLeaf_Angle)
+  heartshape(heart_W_value, heart_H_value)
+  pop()
+  push()
+  rotate(-bigLeaf_Angle)
+  heartshape(heart_W_value, heart_H_value)
+  pop()
 
 
-stroke(color_ResedaGreen)
-fill(color_ResedaGreen )
-push()
-translate(0,heartLeaf_pointY)
-scale(leaf_size_3,leaf_size_3)
-heartshape(heart_W_value,heart_H_value)
-pop()
-push()
-rotate(bigLeaf_Angle)
-scale(leaf_size_3,leaf_size_3)
-heartshape(heart_W_value,heart_H_value)
-pop()
-push()
-rotate(-bigLeaf_Angle)
-scale(leaf_size_3,leaf_size_3)
-heartshape(heart_W_value,heart_H_value)
-pop()
 
 
-}  
+
+  stroke(color_DarkGreen)
+  fill(color_DarkGreen)
+
+  push()
+  translate(0, heartLeaf_pointY)
+  scale(leaf_size_2, leaf_size_2)
+  heartshape(heart_W_value, heart_H_value)
+  pop()
+
+  push()
+
+  rotate(bigLeaf_Angle)
+  scale(leaf_size_2, leaf_size_2)
+  heartshape(heart_W_value, heart_H_value)
+  pop()
+
+  push()
+  rotate(-bigLeaf_Angle)
+  scale(leaf_size_2, leaf_size_2)
+  heartshape(heart_W_value, heart_H_value)
+  pop()
+
+
+  stroke(color_ResedaGreen)
+  fill(color_ResedaGreen)
+  push()
+  translate(0, heartLeaf_pointY)
+  scale(leaf_size_3, leaf_size_3)
+  heartshape(heart_W_value, heart_H_value)
+  pop()
+  push()
+  rotate(bigLeaf_Angle)
+  scale(leaf_size_3, leaf_size_3)
+  heartshape(heart_W_value, heart_H_value)
+  pop()
+  push()
+  rotate(-bigLeaf_Angle)
+  scale(leaf_size_3, leaf_size_3)
+  heartshape(heart_W_value, heart_H_value)
+  pop()
+
+
+}
 
 
 //strawberry crown leaf
-function Leaf(    thinness_value,leafHight_value){
+function Leaf(thinness_value, leafHight_value) {
 
-  let leafHight = map(leafHight_value,0,100,4,10)
-  quad(0,-leafHight,      1.5,-6,     0,0,          -1.5,-6);
+  let leafHight = map(leafHight_value, 0, 100, 4, 10)
+  quad(0, -leafHight, 1.5, -6, 0, 0, -1.5, -6);
 
 
 }
 /*
  * thinness_value ranges from 0-100 and indicates how thin the face is
  */
-function strawBerry(thinness_value,leafHight_value, leafAngle_value,eye_switch_value,mouth_switch_value,petalHight_value,petalWide_value) {
+function strawBerry(thinness_value, leafHight_value, leafAngle_value, eye_switch_value, mouth_switch_value, petalHight_value, petalWide_value) {
   // head
 
   angleMode(DEGREES);
@@ -306,19 +306,19 @@ function strawBerry(thinness_value,leafHight_value, leafAngle_value,eye_switch_v
 
 
 
-let color_DarkGreen = color("#42421c")
-let color_ResedaGreen = color("#84805a")
-let color_Tan = color("#d4b48c")
-let color_IndianRed = color("#bd6868")
-let color_Carmine = color("#950a1e")
-let color_RoseWood = color("#5f0d11")
-let color_Brown = color("#1f0a0b")
-let color_White = color("#fdf5e5")
-let color_Yellow = color("#dbb447")
-let color_PeachYellow = color("#edd596")
+  let color_DarkGreen = color("#42421c")
+  let color_ResedaGreen = color("#84805a")
+  let color_Tan = color("#d4b48c")
+  let color_IndianRed = color("#bd6868")
+  let color_Carmine = color("#950a1e")
+  let color_RoseWood = color("#5f0d11")
+  let color_Brown = color("#1f0a0b")
+  let color_White = color("#fdf5e5")
+  let color_Yellow = color("#dbb447")
+  let color_PeachYellow = color("#edd596")
 
 
-  let leafHight=map(leafHight_value,0,100,1,10)
+  let leafHight = map(leafHight_value, 0, 100, 1, 10)
 
 
 
@@ -332,153 +332,153 @@ let color_PeachYellow = color("#edd596")
 
 
 
-  let= RF_x = map(thinness_value, 0, 100, -head_width/2, 10)
-  let RF_size=map(head_width,0,40,0,random(10,20))
-  let leafAngle=map(leafAngle_value,0,100,40,50)
-  let leafPoint_Y=-2
+  let = RF_x = map(thinness_value, 0, 100, -head_width / 2, 10)
+  let RF_size = map(head_width, 0, 40, 0, random(10, 20))
+  let leafAngle = map(leafAngle_value, 0, 100, 40, 50)
+  let leafPoint_Y = -2
   strokeJoin(ROUND);
 
 
-    
+
   fill(color_Carmine);
   stroke(color_Carmine);
   strokeWeight(8);
-  triangle(-head_width / 2+3, 0, head_width / 2-3, 0, 0 , 8);
+  triangle(-head_width / 2 + 3, 0, head_width / 2 - 3, 0, 0, 8);
 
 
   //strawberry seeds
-let seed_SizeX=10/10
-let seed_SizeY=10/10
-let seed_Number=int(random(1,6))
-let seed_Angle=map(leafAngle_value,0,100,-10,10)
-let seed_x=-1
-let seed_y=-5
+  let seed_SizeX = 10 / 10
+  let seed_SizeY = 10 / 10
+  let seed_Number = int(random(1, 6))
+  let seed_Angle = map(leafAngle_value, 0, 100, -10, 10)
+  let seed_x = -1
+  let seed_y = -5
 
 
-noStroke()
+  noStroke()
 
-fill(color_Yellow)
-
-
-
-
-push()
-
-
-for(let i = 0;i<seed_Number;i++){
-
-  
-rotate(seed_Angle)
-  
+  fill(color_Yellow)
 
 
 
 
-    translate(-seed_SizeX/2, +seed_SizeY/2)
+  push()
 
-  bezier(seed_x -seed_SizeX/2, seed_y+seed_SizeY/2, 
-  seed_x-seed_SizeX*2, seed_y-seed_SizeY,  
-  seed_x+ seed_SizeX, seed_y-seed_SizeY,   
-  seed_x -seed_SizeX/2, seed_y+seed_SizeY/2);
+
+  for (let i = 0; i < seed_Number; i++) {
+
+
+    rotate(seed_Angle)
 
 
 
 
 
+    translate(-seed_SizeX / 2, +seed_SizeY / 2)
+
+    bezier(seed_x - seed_SizeX / 2, seed_y + seed_SizeY / 2,
+      seed_x - seed_SizeX * 2, seed_y - seed_SizeY,
+      seed_x + seed_SizeX, seed_y - seed_SizeY,
+      seed_x - seed_SizeX / 2, seed_y + seed_SizeY / 2);
 
 
-rotate(random(350/seed_Number,320/seed_Number))
 
 
-}
 
-pop()
+
+
+    rotate(random(350 / seed_Number, 320 / seed_Number))
+
+
+  }
+
+  pop()
 
   strokeWeight(1);
   stroke(color_ResedaGreen);
-  fill( color_ResedaGreen);
+  fill(color_ResedaGreen);
 
 
   push()
-  
-  translate (0,leafPoint_Y)
-  scale(1,1)
-  Leaf(    thinness_value,leafHight_value)
+
+  translate(0, leafPoint_Y)
+  scale(1, 1)
+  Leaf(thinness_value, leafHight_value)
   pop()
 
   push()
-  translate (0,leafPoint_Y)
+  translate(0, leafPoint_Y)
   rotate(leafAngle)
 
-  Leaf(    thinness_value,leafHight_value)
+  Leaf(thinness_value, leafHight_value)
   pop()
 
-  push() 
-  translate (0,leafPoint_Y)
+  push()
+  translate(0, leafPoint_Y)
   rotate(-leafAngle)
 
-  Leaf(    thinness_value,leafHight_value)
+  Leaf(thinness_value, leafHight_value)
   pop()
 
 
 
 
-  let eye_switch=eye_switch_value
-  let mouth_switch=mouth_switch_value
+  let eye_switch = eye_switch_value
+  let mouth_switch = mouth_switch_value
 
 
-  let eye_Space=map(leafHight_value,0,100,1.8,2.1)
-  let eye_H=map(leafHight_value,0,100,2.6,5)
-  let eye_W=map(leafHight_value,0,100,1,3)
-  let Nose_H=map(leafHight_value,0,100,1.5,2)
+  let eye_Space = map(leafHight_value, 0, 100, 1.8, 2.1)
+  let eye_H = map(leafHight_value, 0, 100, 2.6, 5)
+  let eye_W = map(leafHight_value, 0, 100, 1, 3)
+  let Nose_H = map(leafHight_value, 0, 100, 1.5, 2)
 
 
- 
-  let mouth_W_2=map(leafHight_value,0,100,1.5,2)
-  let centerY=1.3
-  let mouth_H=map(leafHight_value,0,100,1.5,2)
+
+  let mouth_W_2 = map(leafHight_value, 0, 100, 1.5, 2)
+  let centerY = 1.3
+  let mouth_H = map(leafHight_value, 0, 100, 1.5, 2)
 
 
-// tow round eyes
-    if (eye_switch== 1 || eye_switch == 3) {
-      noStroke()
-      fill(color_Brown);
-      
-      ellipse(-eye_Space, 1 ,eye_H/2);
-      ellipse(eye_Space, 1 ,eye_H/2);
-   
-      
-    }
-  
-//cool eyes  
-    if (eye_switch == 2) {
-      noStroke()
-      fill(color_IndianRed);
+  // tow round eyes
+  if (eye_switch == 1 || eye_switch == 3) {
+    noStroke()
+    fill(color_Brown);
 
-      //nose
-      ellipse(0, Nose_H/2, Nose_H-eye_Space/8,Nose_H/2.9);
-  
-      fill(color_Brown);
-   
-  
-    
-
-      arc(-eye_Space, 0 ,eye_W,eye_H/1.2, 0, 180);
-      arc(eye_Space, 0, eye_W,eye_H/1.2,0, 180);
-    }
+    ellipse(-eye_Space, 1, eye_H / 2);
+    ellipse(eye_Space, 1, eye_H / 2);
 
 
-      // cool mouth
-  if (mouth_switch === 1 ||mouth_switch== 3) {
+  }
+
+  //cool eyes  
+  if (eye_switch == 2) {
+    noStroke()
+    fill(color_IndianRed);
+
+    //nose
+    ellipse(0, Nose_H / 2, Nose_H - eye_Space / 8, Nose_H / 2.9);
+
+    fill(color_Brown);
+
+
+
+
+    arc(-eye_Space, 0, eye_W, eye_H / 1.2, 0, 180);
+    arc(eye_Space, 0, eye_W, eye_H / 1.2, 0, 180);
+  }
+
+
+  // cool mouth
+  if (mouth_switch === 1 || mouth_switch == 3) {
 
     strokeWeight(0.5)
     strokeJoin(ROUND)
     stroke(color_Brown);
     noFill()
     beginShape();
-    vertex(-mouth_W_2/2, mouth_H*2);
-    vertex(0, centerY + 0.5*mouth_H);
-    vertex(mouth_W_2/2, mouth_H*2);
+    vertex(-mouth_W_2 / 2, mouth_H * 2);
+    vertex(0, centerY + 0.5 * mouth_H);
+    vertex(mouth_W_2 / 2, mouth_H * 2);
     endShape();
 
   }
@@ -486,39 +486,39 @@ pop()
   if (mouth_switch >= 2) {
 
 
-  
-  strokeJoin(ROUND)
-  strokeWeight(1.5)
-  //mouth base
-  stroke(color_Carmine);
-  fill(color_Carmine);
-  beginShape();
-  arc(0, centerY+1, mouth_W_2*2, centerY + mouth_H, 0, 180);
-  line(-mouth_W_2,centerY+1,mouth_W_2,centerY+1)
-  endShape();
 
-  noStroke()
+    strokeJoin(ROUND)
+    strokeWeight(1.5)
+    //mouth base
+    stroke(color_Carmine);
+    fill(color_Carmine);
+    beginShape();
+    arc(0, centerY + 1, mouth_W_2 * 2, centerY + mouth_H, 0, 180);
+    line(-mouth_W_2, centerY + 1, mouth_W_2, centerY + 1)
+    endShape();
 
-  ellipse(-mouth_W_2*1.5,centerY+0.9,mouth_H*1)
-  ellipse(mouth_W_2*1.5,centerY+0.9,mouth_H*1)
+    noStroke()
 
-  //real mouth
-  strokeWeight(0.9)
-  stroke(color_IndianRed);
-  fill(color_IndianRed);
-  beginShape();
+    ellipse(-mouth_W_2 * 1.5, centerY + 0.9, mouth_H * 1)
+    ellipse(mouth_W_2 * 1.5, centerY + 0.9, mouth_H * 1)
 
-
-  arc(0, centerY+1, mouth_W_2*2, centerY + mouth_H, 0, 180);
-  line(-mouth_W_2,centerY+1,mouth_W_2,centerY+1)
-  endShape();
+    //real mouth
+    strokeWeight(0.9)
+    stroke(color_IndianRed);
+    fill(color_IndianRed);
+    beginShape();
 
 
+    arc(0, centerY + 1, mouth_W_2 * 2, centerY + mouth_H, 0, 180);
+    line(-mouth_W_2, centerY + 1, mouth_W_2, centerY + 1)
+    endShape();
 
-  strokeWeight(0.1)
-  stroke(color_White);
-  fill(color_White)
-  rect(-mouth_W_2/20, centerY+mouth_H/2, mouth_W_2, mouth_H/4)
+
+
+    strokeWeight(0.1)
+    stroke(color_White);
+    fill(color_White)
+    rect(-mouth_W_2 / 20, centerY + mouth_H / 2, mouth_W_2, mouth_H / 4)
   }
 
 
